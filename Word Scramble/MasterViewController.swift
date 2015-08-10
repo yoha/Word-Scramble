a2351e61daceb65c5249b1e3c8530d22c2111886
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-08-10 21:53:05 +0700

    App is done.

--- Word Scramble/MasterViewController.swift
+++ Word Scramble/MasterViewController.swift
@@ -48,7 +48,6 @@ class MasterViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-
         let userAnswer = self.userAnswers[indexPath.row]
         cell.textLabel!.text = userAnswer
         return cell
@@ -61,6 +60,68 @@ class MasterViewController: UITableViewController {
         self.title = self.allIndividualWords[0]
         self.userAnswers.removeAll(keepCapacity: true)
         self.tableView.reloadData()
+    }
+    
+    func promptForAnswer() {
+        let alertController = UIAlertController(title: "Enter answer...", message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addTextFieldWithConfigurationHandler(nil)
+        let submitAlertAction = UIAlertAction(title: "Submit", style: UIAlertActionStyle.Default) { [unowned self, alertController] (_) -> Void in
+            let answer = alertController.textFields![0]
+            self.submitAnswer(answer.text!)
+        }
+        alertController.addAction(submitAlertAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func submitAnswer(answer: String) {
+        let lowercaseAnswer = answer.lowercaseString
+        if wordIsASubsetOfMaster(lowercaseAnswer) {
+            if wordIsFirstUsed(lowercaseAnswer) {
+                if wordIsValid(lowercaseAnswer) {
+                    self.userAnswers.insert(lowercaseAnswer, atIndex: 0)
+                    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+                    self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+                }
+                else {
+                    self.showErrorMessage("Word not recognized.", errorMessage: "Don't try to fool me. :)")
+                }
+            }
+            else {
+                self.showErrorMessage("That word has been used.", errorMessage: "Be more original! :)")
+            }
+        }
+        else {
+            self.showErrorMessage("That word is not possible", errorMessage: "You can't spell that word from \(self.title!.lowercaseString)!")
+        }
+    }
+    
+    func wordIsASubsetOfMaster(answer: String) -> Bool {
+        var tempMasterWord = self.title!.lowercaseString
+        for eachLetter in answer.characters {
+            if let letterPosition = tempMasterWord.rangeOfString(String(eachLetter)) {
+                if letterPosition.isEmpty { return false }
+                else { tempMasterWord.removeAtIndex(letterPosition.startIndex) }
+            }
+            else { return false }
+        }
+        return true
+    }
+    
+    func wordIsFirstUsed(answer: String) -> Bool {
+        return !self.userAnswers.contains(answer)
+    }
+    
+    func wordIsValid(answer: NSString) -> Bool {
+        let wordChecker = UITextChecker()
+        let wordRange = NSMakeRange(0, answer.length)
+        let misspelledRange = wordChecker.rangeOfMisspelledWordInString(String(answer), range: wordRange, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func showErrorMessage(errorTitle: String, errorMessage: String) {
+        let alertController = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "Got it", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
 
 }
